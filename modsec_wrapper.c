0425e4e8b750c1a9230a11a4bfd3856c14b42099
Maximilian Falkenstein <maximilian.falkenstein@beekeeper.io>
2021-08-05 09:10:14 +0200

    Remove debug comment

--- modsec_wrapper.c
+++ modsec_wrapper.c
@@ -256,11 +256,11 @@ int modsecurity_process(struct worker *worker, struct modsecurity_parameters *pa
 		if (compare(hostname, hostname_len, "10.", 3) == 0 &&
 		    compare(src_ip_z, 9, "::ffff:10", 9) == 0) {
 			// Okay
-			//if (inet_aton(src_ip_z, &host) != 0 && ((ntohl(host.s_addr) & 0x000000FF) == 0x00000010)) {
+			if (inet_aton(src_ip_z, &host) != 0 && ((ntohl(host.s_addr) & 0x000000FF) == 0x00000010)) {
 				printf("Matched modsec rule\n");
 				fail = 0;
 				goto fail;
-			//}
+			}
 		}
 	}
 
